21e9cf21949809433e9771c7d41caaaabc54b9ac
Goheeca <goheeca@gmail.com>
2021-01-19 08:51:07 +0100

    Exported *indent-size* and formatting

--- cl-fxml.lisp
+++ cl-fxml.lisp
@@ -1,7 +1,8 @@
 
 (defpackage :cl-fxml
   (:use :common-lisp :agnostic-lizard :named-readtables)
-  (:export :*new-line-after-opening* :syntax :with-xml))
+  (:export :*new-line-after-opening* :*indent-size*
+	   :syntax :with-xml))
 
 (in-package :cl-fxml)
 
@@ -12,7 +13,7 @@
 
 ;;; Settings
 
-(defconstant +indent-size+ 4)
+(defvar *indent-size* 4)
 (defvar *new-line-after-opening* nil)
 
 ;;; Transformation
@@ -24,19 +25,20 @@
     (destructuring-bind ((tag &rest atts) &body tagbody) form
       `(progn
          (format t "~&~v,v@T<~a" *indentation* *indentation* ,tag)
-         (incf *indentation* +indent-size+)
+         (incf *indentation* *indent-size*)
          ,@(loop for prev-att = nil then att
                  and att in atts
                  collect `(format t ,(cond ((keywordp att) " ~a=")
                                            ((keywordp prev-att) "\"~a\"")
-                                           (t " ~a")) ,att))
+                                           (t " ~a"))
+				  ,att))
          (if *new-line-after-opening*
            (format t ,(if tagbody ">~%" "/>~%"))
-           (format t ,(if tagbody "~&~v,v@T>" "~2*/>~%") *indentation* *indentation* ))
+           (format t ,(if tagbody "~&~v,v@T>" "~2*/>~%") *indentation* *indentation*))
          ,(let ((result (gensym)))
             `(let ((,result ,(funcall walker tagbody)))
                 (when (stringp ,result) (format t (if *new-line-after-opening* "~&~v,v@T~a~%" "~2*~a~%") *indentation* *indentation* ,result))))
-         (decf *indentation* +indent-size+)
+         (decf *indentation* *indent-size*)
          ,(when tagbody `(format t "~&~v,v@T</~a>~%" *indentation* *indentation* ,tag))))
     form))
 
@@ -48,7 +50,7 @@
              collect (agnostic-lizard:walk-form form env 
                        :on-every-form-pre #'(lambda (f e)
                                               (process-xml f #'(lambda (body)
-                                                                    (walk-xml body e))))))))
+                                                                 (walk-xml body e))))))))
 
 ;;; Macro
 
